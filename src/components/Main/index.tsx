ba415474187a1f37df3f7be101267334b6bdaa2d
VitorTheGreat <vitor.services@gmail.com>
2021-11-04 23:01:39 -0300

    added sass to project, ignoring linting to scss and css on pre-commit

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,11 @@
-const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
-)
+import style from 'styles/Main/Main.module.scss'
+
+const Main = () => {
+  return (
+    <main>
+      <h1 className={style.test}>React Avançado</h1>
+    </main>
+  )
+}
 
 export default Main
