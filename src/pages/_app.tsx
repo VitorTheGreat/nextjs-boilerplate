dd5207c676e2940d41b26180e86617f93ddfbf3c
VitorTheGreat <vitor.services@gmail.com>
2021-11-04 21:06:25 -0300

    changing home structure, lint, added title tag, changed prettier file name, removed styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
-import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Next JS App - Boilerplate</title>
+        <meta
+          name="description"
+          content="Next JS App - Boilerplate (Say NO to styled-components)"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
