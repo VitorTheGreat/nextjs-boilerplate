8386733ea001f6402599c7b5012d0ed6cb667a90
VitorTheGreat <vitor.services@gmail.com>
2021-11-10 22:41:19 -0300

    added sass to project, corrected jest to work with sass and updated snapshots

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-    //jsdom is to simulate the browser, in order to run correctly the tests
-    testEnvironment: "jsdom",
-    testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-    //to check if we are covering all tests - functions, variables, etc...
-    collectCoverage: true,
-    //types of files to cover
-    collectCoverageFrom: ["src/**/*.ts(x)"],
-    setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  //jsdom is to simulate the browser, in order to run correctly the tests
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  //to check if we are covering all tests - functions, variables, etc...
+  collectCoverage: true,
+  //types of files to cover
+  collectCoverageFrom: ['src/**/*.ts(x)'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '\\.module\\.(css|scss)$': 'identity-obj-proxy'
+  }
 }
